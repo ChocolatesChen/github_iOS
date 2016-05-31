b2972b6cc43f0177fd40763ad6f37a788f820d2c
ChocolatesChen <chenguang_defu@163.com>
2016-05-31 09:39:37 +0800

    init

---
+++ github_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github_iOS
+//
+//  Created by Mac on 16/5/31.
+//  Copyright © 2016年 Chocolate_Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ github_iOS/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  github_iOS
+//
+//  Created by Mac on 16/5/31.
+//  Copyright © 2016年 Chocolate_Chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    // This is initialize creation（这是一个初始化创建）
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
